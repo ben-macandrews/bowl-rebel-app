ece057baa10555cf438e366228dce544ebfe3c5a
Ben MacAndrews <benmacandrews@Bens-MacBook-Pro-2.local>
2022-01-29 16:01:39 +0000

    Add javascript interactivity for nav-buttons based on class of slideshow images (empathise, define, ideate...)

--- resources/script.js
+++ resources/script.js
@@ -1,4 +1,4 @@
-var slideIndex = 1;
+        var slideIndex = 1;
         showSlides(slideIndex);
         
         function plusSlides(n) {
@@ -23,4 +23,34 @@ var slideIndex = 1;
           }
           slides[slideIndex-1].style.display = "block";  
           dots[slideIndex-1].className += " active";
+          
+            // Nav Button Interactivity
+
+            var navIndex = 1;
+            activeButtons(navIndex);
+
+            function activeButtons (n) {
+                var navButtons = document.getElementsByClassName("nav-buttons");
+                var empathiseSlides = document.getElementsByClassName("empathise");
+                var defineSlides = document.getElementsByClassName("define");
+                var ideateSlides = document.getElementsByClassName("ideate");
+                var prototypeSlides = document.getElementsByClassName("prototype");
+                var testSlides = document.getElementsByClassName("test");
+                
+                if (slideIndex >= 1 && slideIndex < (empathiseSlides.length + 1)) {
+                    navIndex = 1;
+                } else if (slideIndex >= (empathiseSlides.length + 1) && slideIndex < (empathiseSlides.length + defineSlides.length + 1)) {
+                    navIndex = 2;
+                } else if (slideIndex >= (empathiseSlides.length + defineSlides.length + 1) && slideIndex < (empathiseSlides.length + defineSlides.length + ideateSlides.length + 1)) {
+                    navIndex = 3;
+                } else if (slideIndex >= (empathiseSlides.length + defineSlides.length + ideateSlides.length + 1) && slideIndex < (empathiseSlides.length + defineSlides.length + ideateSlides.length + prototypeSlides.length+ 1)) {
+                    navIndex = 4;
+                }
+                for (i = 0; i < navButtons.length; i++) {
+                    navButtons[i].className = navButtons[i].className.replace(" active-nav", "");
+                }
+                navButtons[navIndex-1].className += " active-nav";
+            }
+
         }
+
